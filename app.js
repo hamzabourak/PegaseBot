2bdc0d59cfcf46ccc7fdb7556a0377c2b2e7d571
hamzabourak <bourakhamza@gmail.com>
2016-10-14 17:10:57 +0200

    bof

--- app.js
+++ app.js
@@ -19,10 +19,10 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
   
-// Create chat bot
+//Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.APP_ID,
-    appPassword: process.env.APP_PWD
+    appId: 'f252e82e-f9e7-4560-8c90-efa47bd5fa55',
+    appPassword: 'daSuXrGE1PtRdvLP0HWVvEd'
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
@@ -31,18 +31,26 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
+
+bot.dialog('/', [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your name to %s', session.userData.name);
+    }
+]);
 
 
-//Swith case dialogs
-// bot.dialog('/', new builder.IntentDialog()
-//     .matches(/^add/i, '/addTask')
-//     .matches(/^change/i, '/changeTask')
-//     .matches(/^delete/i, '/deleteTask')
-//     .onDefault(builder.DialogAction.send("I'm sorry. I didn't understand.")));
-
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.choice(session, "Which color?", "red|green|blue");
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
 
 
 
